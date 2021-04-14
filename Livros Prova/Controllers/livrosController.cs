f0ef8468998c38a1771489114272949640bec4df
lfifolato <ldalri1999@gmail.com.br>
2021-04-14 00:07:00 -0300

    Adicionar arquivos de projeto.

---
+++ Livros Prova/Controllers/livrosController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Livros_Prova.Data;
+using Livros_Prova.Models;
+
+namespace Livros_Prova.Controllers
+{
+    public class livrosController : Controller
+    {
+        private readonly DataContext _context;
+
+        public livrosController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: livros
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Livros.ToListAsync());
+        }
+
+        // GET: livros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+
+            return View(livros);
+        }
+
+        // GET: livros/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: livros/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,titulo,descricao,autor,quantPag,editora,datalancamento,preco")] livros livros)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(livros);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livros);
+        }
+
+        // GET: livros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros.FindAsync(id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+            return View(livros);
+        }
+
+        // POST: livros/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,titulo,descricao,autor,quantPag,editora,datalancamento,preco")] livros livros)
+        {
+            if (id != livros.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livros);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!livrosExists(livros.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livros);
+        }
+
+        // GET: livros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var livros = await _context.Livros
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livros == null)
+            {
+                return NotFound();
+            }
+
+            return View(livros);
+        }
+
+        // POST: livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var livros = await _context.Livros.FindAsync(id);
+            _context.Livros.Remove(livros);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool livrosExists(int id)
+        {
+            return _context.Livros.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Livros Prova/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Livros_Prova.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Livros_Prova.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<livros> Livros { get; set; }
+
+    }
+}
